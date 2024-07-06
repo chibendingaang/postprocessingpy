846f80518a76a1e701e78e07f307c6e418989183
chibendingaang <foundnisarg28@gmail.com>
2024-07-07 03:21:57 +0530

    Create translate_xt_axes.py
    Dimensional consistency for a sliced array

---
+++ translate_xt_axes.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+np.random.seed(98)
+L = 8, steps = 48
+arr = np.random.uniform(-1,1,(steps+1, L,3))
+a,b,c = arr.shape
+
+# we are checking that the dimension of the first x and first y sliced elements 
+# of the array is the same as the last x and the last y sliced elements 
+for x in range(a):
+    for y in range(b):
+        K1 = arr[x:, y:, :]
+        K2 = arr[:a-x, :b-y, :]
+        print(K1.shape, K2.shape)
