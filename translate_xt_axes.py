2b63fa2fead3bd7d49d97d237e512d1caea6e5f8
chibendingaang <foundnisarg28@gmail.com>
2024-07-07 03:25:18 +0530

    Update translate_xt_axes.py

--- translate_xt_axes.py
+++ translate_xt_axes.py
@@ -7,8 +7,8 @@ a,b,c = arr.shape
 
 # we are checking that the dimension of the first x and first y sliced elements 
 # of the array is the same as the last x and the last y sliced elements 
-for x in range(a):
-    for y in range(b):
+for x in range(a+1):
+    for y in range(b+1):
         K1 = arr[x:, y:, :]
         K2 = arr[:a-x, :b-y, :]
         print(K1.shape, K2.shape)
