674610cf8f4d14e4cd97f45c645073f46b78857f
chibendingaang <foundnisarg28@gmail.com>
2024-07-15 11:34:10 +0530

    Update plot_decorravg.py

--- plot_decorravg.py
+++ plot_decorravg.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 13 17:46:47 2021
-
 @author: nisarg
 """
 import sys
@@ -16,15 +14,25 @@ dtsymb = int(sys.argv[2])
 dt = 0.001 *dtsymb
 
 #int(L*0.05/dt)//32 #25*L//32
-Lambda, Mu, begin, end, epss = map(int,sys.argv[3:8])
+Lambda, Mu = map(float, sys.argv[3:5])
+begin, end, epss = map(int,sys.argv[5:8])
 
 threshfactor = 100
 interval = 1 #interval is same as fine_res
+param = 'xpa2b0'; #'xpdrvn'; 'xphsbg'
 
+inv_dtfact = int(1/(0.1*dtsymb))
 dtstr = f'{dtsymb}emin3'
 epsilon = 10**(-1.0*epss) #0.00001
-epstr = dict()
-epstr[epss] = f'emin{epss}'
+
+alpha = (Lambda - Mu)/(Lambda + Mu)
+alphadeci = lambda alpha: ('0' + str(int(100*(alpha%1)))) if (int(100*(alpha%1)) < 10) else (str(int(100*(alpha%1))))
+alpha_deci = alphadeci(alpha)
+alphastr_ = lambda alpha: str(int(alpha/1)) + 'pt' + alpha_deci
+alphastr = alphastr_(alpha)
+print('alphastr = ', alphastr) 
+
+epstr = {3:'emin3', 4:'emin4', 6:'emin6', 8:'emin8', 33:'min3', 44:'min4'}
 
 if Lambda == 1 and Mu == 0: param = 'qwhsbg'
 if Lambda == 0 and Mu == 1: param = 'qwdrvn'
@@ -34,22 +42,24 @@ if Lambda == 1 and Mu == 1:
     path = f'Dxt_storage/{param}' 
     #{epstr[epss]}_{param}'
 else:
-    path = f'Dxt_storage/{epstr[epss]}_{param}'
+    path = f'Dxt_storage/alpha_ne_pm1' #{epstr[epss]}_{param}'
 
 #files where Dxt_ arrays are stored at
-filerepo = f'./{path}/{param}_dxt_{dtstr}_{epstr[epss]}_out.txt'
+filerepo = f'./{path}/{param}_dxt_{dtstr}_{alphastr}_out.txt'
 f = open(filerepo)
 Dxt_files = np.array([dxtarr for dxtarr in f.read().splitlines()])[begin:end]
 f.close()
-print(Dxt_files)
+print(Dxt_files[::10])
 configs = Dxt_files.shape[0]
-print('# configs = ', configs)
 
-steps = 1280*int(L/1024)//dtsymb #standard number of steps that have been input for the simulation runs
-#what if L = 512? avoid integer division unless necessary
+steps = int(1280*L/1024)//dtsymb 
+#standard number of steps that have been input for the simulation runs
 #either 1280 or 1600 as prefactor
-Dxtavg = np.zeros((2*steps+1,L)) #2*steps+1
 
+if L == 512: steps = 512 #min(512, steps)
+Dxtavg = np.zeros((inv_dtfact*steps+1,L)) #2*steps+1
+print('Dxtavg.shape: ', Dxtavg.shape)
+print('steps : ', steps)
 
 for k, fk  in enumerate(Dxt_files):
     if dtsymb==1: 
@@ -60,11 +70,14 @@ for k, fk  in enumerate(Dxt_files):
     if dtsymb==2:
         if param == 'qwdrvn': Dxtk = np.load(f'./{path}/101M_2emin3/{fk}', allow_pickle=True);
         elif param == 'qwhsbg': Dxtk = np.load(f'./{path}/81M_2emin3/{fk}', allow_pickle=True);
-    Dxtavg += Dxtk[:2*steps+1]
+        else: Dxtk = np.load(f'./{path}/{fk}', allow_pickle=True)
+    Dxtavg += Dxtk #[:inv_dtfact*steps+1]
     # why 2*steps + 1: dtsymb = 2*10**(-3); 
     # it is stored every 100 steps in dynamics --> 0.2; 5 time steps per unit time retrieved
 
 Dxtavg = Dxtavg/len(Dxt_files)
+print(Dxtavg[::100,:])
+print(Dxtavg.shape)
 #D_th = 100*epsilon**2		#not needed 
 
 t_,x_ = Dxtavg.shape
@@ -83,13 +96,13 @@ ax.set_xlabel(r'$\mathbf{x}$')
 ax.set_ylabel(r'$\mathbf{t}$')
 #ax.set_title(r'$ \lambda = $ {}, $\mu = $ {}'.format(Lambda,Mu),fontsize=16) #$D(x,t)$ heat map; $t$ v/s $x$;
 #xticks = ticker.MaxNLocator(7)
-xticks = np.array([-960, -640, -320, 0, 320, 640, 960])
-ax.set_xticks(xticks)
+#xticks = np.array([-960, -640, -320, 0, 320, 640, 960])
+#ax.set_xticks(xticks)
 
 #plt.colorbar()
 fig.colorbar(img, ax=ax,shrink=0.8 ) #, location='left')
 fig.tight_layout()
-plt.savefig(f'./plots/Dxt_L{L}_{param}_{epstr[epss]}_dt_{dtstr}_{configs}configs.png')
+plt.savefig(f'./plots/Dxt_L{L}_{param}_{alphastr}_dt_{dtstr}_{configs}configs.png')
 #plt.show()
 
 """
